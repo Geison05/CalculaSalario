4c0556ad675df15a85993cc20042900e0b2d8562
Geison Amorim <geisonam43@gmail.com>
2022-04-11 22:27:09 -0300

    Add project files.

---
+++ CalculaSalario/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variavel
+
+            int numero, horas;
+            double valorHora, salario;
+
+            //Entrada 
+            numero = int.Parse(Console.ReadLine());
+            horas = int.Parse(Console.ReadLine());
+            valorHora = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            //Processamento
+
+            salario = valorHora * horas;
+
+            //Saida
+
+            Console.WriteLine($"NUMERO = {numero}");
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2",CultureInfo.InvariantCulture));
+            
+        }
+    }
+}
